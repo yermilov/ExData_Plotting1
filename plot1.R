caf80bede59cc47e8af2b886bb84b2e11347b7db
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-11 13:27:47 +0200

    plot #1

---
+++ plot1.R
@@ -0,0 +1,7 @@
+data <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'), na.strings = '?')
+data <- data[data[, 'Date'] == '1/2/2007' | data[, 'Date'] == '2/2/2007', ]
+
+hist(data$'Global_active_power', col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency"))
+
+dev.copy(png, file = "plot1.png") 
+dev.off()
