5537a1d1ecd3a746dfda2f9ced719c96928bb98c
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-11 13:51:35 +0200

    add date/time parsing

--- plot1.R
+++ plot1.R
@@ -1,5 +1,6 @@
 data <- read.table("data/household_power_consumption.txt", header = TRUE, sep = ";", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'), na.strings = '?')
 data <- data[data[, 'Date'] == '1/2/2007' | data[, 'Date'] == '2/2/2007', ]
+data$'Datetime' <- strptime(paste(data$'Date', data$'Time'), format = "%d/%m/%Y %H:%M:%S")
 
 hist(data$'Global_active_power', col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency"))
 
